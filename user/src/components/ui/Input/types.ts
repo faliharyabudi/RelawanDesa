216f04eff2dd509a0dd9bcc179365687234ae851
faliharyabudi <iniarya6@gmail.com>
2026-06-12 20:27:32 +0700

    feat(components): add Input component types

---
+++ user/src/components/ui/Input/types.ts
@@ -0,0 +1,8 @@
+import { TextInputProps, StyleProp, ViewStyle } from 'react-native';
+export interface InputProps extends TextInputProps {
+  label?: string;
+  error?: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  containerStyle?: StyleProp<ViewStyle>;
+}
