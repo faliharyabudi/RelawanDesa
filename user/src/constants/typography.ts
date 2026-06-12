99971adb4e2afbf13713751e888b7fbff459fb8a
faliharyabudi <iniarya6@gmail.com>
2026-06-12 20:27:17 +0700

    refactor(theme): extract typography configurations

---
+++ user/src/constants/typography.ts
@@ -0,0 +1,10 @@
+export const typography = {
+  fonts: {
+    regular: 'PlusJakartaSans_400Regular',
+    medium: 'PlusJakartaSans_500Medium',
+    semiBold: 'PlusJakartaSans_600SemiBold',
+    bold: 'PlusJakartaSans_700Bold',
+    extraBold: 'PlusJakartaSans_800ExtraBold',
+  },
+  sizes: { xs: 12, sm: 14, md: 16, lg: 18, xl: 20, xxl: 24, xxxl: 30 }
+};
