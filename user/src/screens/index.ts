5b72002cde17e9e35d996ee893cf08a74960d565
faliharyabudi <iniarya6@gmail.com>
2026-06-12 20:27:53 +0700

    refactor(screens): add screen exports index file for cleaner imports

---
+++ user/src/screens/index.ts
@@ -0,0 +1,4 @@
+export { default as HomeScreen } from './HomeScreen';
+export { default as ProfileScreen } from './ProfileScreen';
+export { default as HistoryScreen } from './HistoryScreen';
+export { default as LoginScreen } from './LoginScreen';
